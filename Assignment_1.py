c86385455fd90e021519db13effe97836ec31717
Adarsh390 <117708484+Adarsh390@users.noreply.github.com>
2022-11-08 11:27:53 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,30 @@
+##Problem1:-
+print("Hello Python")
+
+
+##Problem2:-
+a = 20
+b = 5
+add =  a+b
+div = a/b
+print(add)
+print(div)
+
+
+##Problem3:-
+height = float(input("Enter the height of Triangle:- "))
+base = float(input("Enter the base of the Triangle:- "))
+area = (height * base)/2 
+print(area)
+
+##Problem4:-
+var1 = 10
+var2 = 5
+var1 = var1+var2
+var2 = var1-var2
+var1 = var1-var2
+print(var1,var2)
+
+##Problem5:-
+import random
+print(random.randint(1,1000))   
